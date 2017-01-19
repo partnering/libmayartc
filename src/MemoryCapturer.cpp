8503890b53dd363f1e20b7857afc5daa0ea0ea68
Sylvain MAHE <sylvain.mahe@partnering.fr>
2017-01-19 16:09:20 +0100

    fixes indentation

--- src/MemoryCapturer.cpp
+++ src/MemoryCapturer.cpp
@@ -1,9 +1,9 @@
 /*
-* MemoryCapturer.cpp
-*
-*  Created on: 07 mar 2016
-*      Author: jfellus
-*/
+ * MemoryCapturer.cpp
+ *
+ *  Created on: 07 mar 2016
+ *      Author: jfellus
+ */
 #include <iostream>
 
 #include "MemoryCapturer.hpp"
@@ -16,74 +16,74 @@ namespace maya {
 
 
 
-MemoryCapturer::MemoryCapturer(rtc::Thread* thread, uint w, uint h)
-	: cricket::VideoCapturer(), running_(false),
-	initial_unix_timestamp_(time(NULL) * rtc::kNumNanosecsPerSec),
-	next_timestamp_(rtc::kNumNanosecsPerMillisec),
-	is_screencast_(false),
-	rotation_(webrtc::kVideoRotation_0) {
+	MemoryCapturer::MemoryCapturer(rtc::Thread* thread, uint w, uint h)
+		: cricket::VideoCapturer(), running_(false),
+		initial_unix_timestamp_(time(NULL) * rtc::kNumNanosecsPerSec),
+		next_timestamp_(rtc::kNumNanosecsPerMillisec),
+		is_screencast_(false),
+		rotation_(webrtc::kVideoRotation_0) {
 
-	std::vector<cricket::VideoFormat> formats;
-	formats.push_back(cricket::VideoFormat(w, h, cricket::VideoFormat::FpsToInterval(30), cricket::FOURCC_ARGB));
-	ResetSupportedFormats(formats);
-}
-
-MemoryCapturer::~MemoryCapturer() {
-	SignalDestroyed(this);
-}
-
-bool MemoryCapturer::CaptureFrame(const char* img, int w, int h) {
-	if (!GetCaptureFormat()) {
-		return false;
-	}
-	if (!running_) return false;
-
-	size_t size = w*h*4;
-
-	cricket::CapturedFrame frame;
-	frame.width = w;
-	frame.height = h;
-	frame.fourcc = cricket::FOURCC_ARGB;
-	frame.data_size = size;
-	frame.time_stamp = initial_unix_timestamp_ + next_timestamp_;
-	next_timestamp_ += 33333333;
-
-	char* buf = new char[size];
-	for(uint y = 0; y<h; y++) {
-		for(uint x=0; x<w; x++) {
-			buf[(y*w+x)*4+2] = img[(y*w+x)*3];
-			buf[(y*w+x)*4+1] = img[(y*w+x)*3+1];
-			buf[(y*w+x)*4] = img[(y*w+x)*3+2];
+			std::vector<cricket::VideoFormat> formats;
+			formats.push_back(cricket::VideoFormat(w, h, cricket::VideoFormat::FpsToInterval(30), cricket::FOURCC_ARGB));
+			ResetSupportedFormats(formats);
 		}
+
+	MemoryCapturer::~MemoryCapturer() {
+		SignalDestroyed(this);
 	}
 
-	std::unique_ptr<char[]> data(buf);
-	frame.data = data.get();
-	frame.rotation = rotation_;
+	bool MemoryCapturer::CaptureFrame(const char* img, int w, int h) {
+		if (!GetCaptureFormat()) {
+			return false;
+		}
+		if (!running_) return false;
 
-	SignalFrameCaptured(this, &frame);
-	return true;
-}
+		size_t size = w*h*4;
+
+		cricket::CapturedFrame frame;
+		frame.width = w;
+		frame.height = h;
+		frame.fourcc = cricket::FOURCC_ARGB;
+		frame.data_size = size;
+		frame.time_stamp = initial_unix_timestamp_ + next_timestamp_;
+		next_timestamp_ += 33333333;
+
+		char* buf = new char[size];
+		for(uint y = 0; y<h; y++) {
+			for(uint x=0; x<w; x++) {
+				buf[(y*w+x)*4+2] = img[(y*w+x)*3];
+				buf[(y*w+x)*4+1] = img[(y*w+x)*3+1];
+				buf[(y*w+x)*4] = img[(y*w+x)*3+2];
+			}
+		}
+
+		std::unique_ptr<char[]> data(buf);
+		frame.data = data.get();
+		frame.rotation = rotation_;
+
+		SignalFrameCaptured(this, &frame);
+		return true;
+	}
 
 
- cricket::CaptureState MemoryCapturer::Start(const cricket::VideoFormat& format) {
-	cricket::VideoFormat supported;
-	if (GetBestCaptureFormat(format, &supported)) SetCaptureFormat(&supported);
-	running_ = true;
-	 SetCaptureState(cricket::CS_RUNNING);
-	return cricket::CS_RUNNING;
-}
+	cricket::CaptureState MemoryCapturer::Start(const cricket::VideoFormat& format) {
+		cricket::VideoFormat supported;
+		if (GetBestCaptureFormat(format, &supported)) SetCaptureFormat(&supported);
+		running_ = true;
+		SetCaptureState(cricket::CS_RUNNING);
+		return cricket::CS_RUNNING;
+	}
 
- void MemoryCapturer::Stop() {
-	running_ = false;
-	SetCaptureFormat(NULL);
-	SetCaptureState(cricket::CS_STOPPED);
-}
+	void MemoryCapturer::Stop() {
+		running_ = false;
+		SetCaptureFormat(NULL);
+		SetCaptureState(cricket::CS_STOPPED);
+	}
 
-bool MemoryCapturer::GetPreferredFourccs(std::vector<uint32_t>* fourccs) {
-	fourccs->push_back(cricket::FOURCC_ARGB);
-	return true;
-}
+	bool MemoryCapturer::GetPreferredFourccs(std::vector<uint32_t>* fourccs) {
+		fourccs->push_back(cricket::FOURCC_ARGB);
+		return true;
+	}
 
 
 
--- src/MemoryCapturer.hpp
+++ src/MemoryCapturer.hpp
@@ -20,34 +20,34 @@ namespace maya {
 	class MemoryCapturer : public cricket::VideoCapturer {
 
 
-	public:
-     MemoryCapturer(rtc::Thread* thread, uint w, uint h);
-     ~MemoryCapturer();
+		public:
+			MemoryCapturer(rtc::Thread* thread, uint w, uint h);
+			~MemoryCapturer();
 
-     void ResetSupportedFormats(const std::vector<cricket::VideoFormat>& formats) { SetSupportedFormats(formats); }
-     bool CaptureFrame(const char* img, int w, int h);
+			void ResetSupportedFormats(const std::vector<cricket::VideoFormat>& formats) { SetSupportedFormats(formats); }
+			bool CaptureFrame(const char* img, int w, int h);
 
-     void SignalCapturedFrame(cricket::CapturedFrame* frame) { SignalFrameCaptured(this, frame); }
+			void SignalCapturedFrame(cricket::CapturedFrame* frame) { SignalFrameCaptured(this, frame); }
 
-     sigslot::signal1<MemoryCapturer*> SignalDestroyed;
+			sigslot::signal1<MemoryCapturer*> SignalDestroyed;
 
-     virtual cricket::CaptureState Start(const cricket::VideoFormat& format);
-     virtual void Stop();
-     virtual bool IsRunning() { return running_; }
-     void SetScreencast(bool is_screencast) { is_screencast_ = is_screencast; }
-     virtual bool IsScreencast() const { return is_screencast_; }
-     bool GetPreferredFourccs(std::vector<uint32_t>* fourccs);
+			virtual cricket::CaptureState Start(const cricket::VideoFormat& format);
+			virtual void Stop();
+			virtual bool IsRunning() { return running_; }
+			void SetScreencast(bool is_screencast) { is_screencast_ = is_screencast; }
+			virtual bool IsScreencast() const { return is_screencast_; }
+			bool GetPreferredFourccs(std::vector<uint32_t>* fourccs);
 
-     void SetRotation(webrtc::VideoRotation rotation) { rotation_ = rotation; }
-     webrtc::VideoRotation GetRotation() { return rotation_; }
+			void SetRotation(webrtc::VideoRotation rotation) { rotation_ = rotation; }
+			webrtc::VideoRotation GetRotation() { return rotation_; }
 
-    private:
-     bool running_;
-     int64_t initial_unix_timestamp_;
-     int64_t next_timestamp_;
-     bool is_screencast_;
-     webrtc::VideoRotation rotation_;
-   };
+		private:
+			bool running_;
+			int64_t initial_unix_timestamp_;
+			int64_t next_timestamp_;
+			bool is_screencast_;
+			webrtc::VideoRotation rotation_;
+	};
 
 
 }
